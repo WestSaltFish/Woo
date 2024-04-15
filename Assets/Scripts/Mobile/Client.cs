5efac24b1e80ebd71848e2c0aab0a65030b21e20
Yogui <zhidbcn@gmail.com>
2024-04-15 17:38:13 +0200

    first push

---
+++ Assets/Scripts/Mobile/Client.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    public uint uid;
+
+    public string name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
